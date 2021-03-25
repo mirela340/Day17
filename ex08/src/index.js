9ef83b5ba5d37295ec2e9c3004c7be8542ef00a3
Mirel Slamnik <slamnik.mirela@hotmail.com>
2021-03-25 09:54:06 +0100

    first commit

---
+++ ex08/src/index.js
@@ -0,0 +1,28 @@
+// Lodash
+const_ = require('lodash');
+// only change code below this line
+// users nested array with four objects starts here
+
+// users nested array with four objects ends here
+
+
+
+// //getUsers function - list of users starts here
+
+// getUsers function - list of users ends here
+// findUser(lastName, gender) function starts here
+function findUser(lastName, gender) {
+    try {
+        // add apropriate code here
+
+    } catch (error) {
+        return // Change this line
+        console.log(); // Change this line
+    }
+}
+// findUser(lastName, gender) function ends here
+//   Only change code above this line
+getUsers();
+findUser(); // Change this line
+
+module.exports = findUser;
---
+++ ex09/src/index.js
@@ -0,0 +1,28 @@
+// Lodash
+const_ = require('lodash');
+// only change code below this line
+// users nested array with four objects starts here
+
+// users nested array with four objects ends here
+
+
+
+// //getUsers function - list of users starts here
+
+// getUsers function - list of users ends here
+// findUser(lastName, gender) function starts here
+function findUserById(Id) {
+    try {
+        // add apropriate code here
+
+    } catch (error) {
+        return // Change this line
+        console.log(); // Change this line
+    }
+}
+// findUser(lastName, gender) function ends here
+//   Only change code above this line
+getUsers();
+findUser(); // Change this line
+
+module.exports = findUserById;
